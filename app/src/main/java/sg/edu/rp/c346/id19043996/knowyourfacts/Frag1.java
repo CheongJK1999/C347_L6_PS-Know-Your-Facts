ea4c9b0efda4f2a8c466bb9691cb552b993d30b9
LI SHUFANG <lishufang1121@gmail.com>
2021-05-27 16:36:51 +0800

    Shufang commit

--- app/src/main/java/sg/edu/rp/c346/id19043996/knowyourfacts/Frag1.java
+++ app/src/main/java/sg/edu/rp/c346/id19043996/knowyourfacts/Frag1.java
@@ -56,7 +56,8 @@ public class Frag1 extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frag1, container, false);
