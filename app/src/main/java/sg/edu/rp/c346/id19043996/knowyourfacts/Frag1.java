34fa6cda059b8774e186c0f26c16f0417db0ccc2
LI SHUFANG <Problem Statement>
2021-05-27 23:56:25 +0800

    final commit

--- app/src/main/java/sg/edu/rp/c346/id19043996/knowyourfacts/Frag1.java
+++ app/src/main/java/sg/edu/rp/c346/id19043996/knowyourfacts/Frag1.java
@@ -9,15 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.Random;
 
 // Todo by Jun Kai
 public class Frag1 extends Fragment {
-
+    ImageView iv1;
     TextView tv1, tv2, tv3;
     Button btnColor;
+    LinearLayout linearlayout1;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -29,7 +34,11 @@ public class Frag1 extends Fragment {
         tv1 = view.findViewById(R.id.tv1);
         tv2 = view.findViewById(R.id.tv2);
         tv3 = view.findViewById(R.id.tv3);
+        iv1 = view.findViewById(R.id.iv);
         btnColor = view.findViewById(R.id.btnColor);
+
+        String imageUrl = "https://wtffunfact.com/wp-content/uploads/2021/05/WTF-Fun-Fact-Arctic-Foxs-Colorful-Fur.png";
+        Picasso.with(view.getContext()).load(imageUrl).into(iv1);
 
         btnColor.setOnClickListener(new View.OnClickListener() {
             @Override
